c41c7c570d3b1294e3d2c449a01e9657a02a71ef
styley997 <styley997@gmail.com>
2019-04-01 19:53:07 +0700

    add employee & project fleid

--- Trip/admin.py
+++ Trip/admin.py
@@ -5,8 +5,16 @@ import nested_admin
 from .models import Trip
 from .models import TripDetail
 from .models import Evidence
+from .models import Employee
+from .models import Project
 from django.utils import timezone
 
+
+class EmployeeAdmin(nested_admin.NestedModelAdmin):
+    fields = ('employee_name' ,)
+
+class ProjectAdmin(nested_admin.NestedModelAdmin):
+    fields = ('project_name' ,)
 
 class EvidencelInline(nested_admin.NestedStackedInline):
     fields = ('evidence_detail','extra_cost',)
@@ -21,15 +29,45 @@ class TripDetailInline(nested_admin.NestedStackedInline):
     extra = 0
     
 class TripAdmin(nested_admin.NestedModelAdmin):
-    list_display = ('employee_name', 'project_name','calculate_cost', 'post_date')
-    readonly_fields = ('calculate_cost',)
+    list_display = ('get_employee_name', 'get_project_name','calculate_cost', 'post_date')
+    readonly_fields = ('calculate_cost','calculate_extra',)
+
+    # fieldsets = (
+    #   ('Standard info', {
+    #       'fields': ('name')
+    #   }),
+    #   ('Address info', {
+    #       'fields': ('address', ('city', 'zip'))
+    #   }),
+    # )
     def calculate_cost(self, obj):
         details = obj.tripdetail_set.all()
         base = 0
         for detail in details:
             base = base + detail.cost
         return base
+        
+    def calculate_extra(self,obj):
+        extras = obj.tripdetail_set.all().Evidence_set.all()
+        total_extra = 0
+        for extra in extras:
+            total_extra = total_extra + extra.extra_cost
+        return total_extra
+        
+    def get_employee_name(self, obj):
+        return obj.employee.employee_name
+    get_employee_name.short_description = 'Employee name'
+    get_employee_name.admin_order_field = 'employee__employee_name'
+
+    def get_project_name(self, obj):
+        return obj.project.project_name
+    get_project_name.short_description = 'Project name'
+    get_project_name.admin_order_field = 'project__project_name'
+
     calculate_cost.short_description = 'calculate cost'
     inlines = (TripDetailInline,)
 
 admin.site.register(Trip,TripAdmin)
+
+admin.site.register(Employee,EmployeeAdmin)
+admin.site.register(Project,ProjectAdmin)
--- Trip/models.py
+++ Trip/models.py
@@ -7,14 +7,22 @@ from django.contrib.auth.models import User
 
 
 # Create your models here:
-class Trip(models.Model):
+class Employee(models.Model):
     employee_name = models.CharField(max_length=200, default="")
-    post_date = models.DateTimeField('post date', auto_now_add=True)
-    project_name = models.CharField(max_length=200, default="")
-# class EmployeeInformation(models.Model):
-#     employee_name = models.CharField(max_length=200)
     def __str__(self):
          return self.employee_name
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200, default="")
+    def __str__(self):
+         return self.project_name
+
+class Trip(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE,default = None, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,default = None, null = True)
+
+    post_date = models.DateTimeField('post date', auto_now_add=True)
+
 
 class TripDetail(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE,default = None)
@@ -24,6 +32,9 @@ class TripDetail(models.Model):
     distance = models.FloatField(default=0)
     trip_date = models.DateTimeField('Trip date', default=None)
 
+    def __str__(self):
+         return self.start
+
     @property
     def cost(self):
         return self.distance * 8
@@ -31,8 +42,10 @@ class TripDetail(models.Model):
 
 class Evidence(models.Model):
     TripDetail = models.ForeignKey(TripDetail, on_delete=models.CASCADE,default = None)
-
+    
     evidence_detail = models.CharField(max_length=200)
     extra_cost= models.FloatField(default=0)
+
     def __str__(self):
         return self.evidence_detail
+        
